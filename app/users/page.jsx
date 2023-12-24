d9ea8d267dce146d3730ede32a86efa862a456c1
Nishan4477 <nishansakha@gmail.com>
2023-12-24 21:41:21 +0545

    heading added

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import AccordionUI from '@/components/AccordionUI'
 
 
+
 const users = () => {
   return (
     <div className='flex flex-col justify-center items-center mt-10'>
+      <h2 className="font-bold text-4xl mb-4 ">USER MANAGEMENT SYSTEM</h2>
         <AccordionUI/>
     </div>
   )
